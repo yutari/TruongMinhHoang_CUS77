e07479d97d82743fdc86c3d87193c5f6ab4a8089
yutari <minhhoangtruong19@gmail.com>
2022-10-18 21:20:59 +0700

    kk

---
+++ lesson2/src/components/card/index.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+<div id="app">
+  <div class="card">
+    <div class="name">Name: Alice</div>
+    <div class="age">Age: 20</div>
+  </div>
+  <div class="card">
+    <div class="name">Name: Bob</div>
+    <div class="age">Age: 20</div>
+  </div>
+  <div class="card">
+    <div class="name">Name: Cris</div>
+    <div class="age">Age: 20</div>
+  </div>
+</div>
